4fd2425ca3a0e110b8587feeccc4734941899d7c
klobukov <klobukov.vladimir@rambler.ru>
2021-03-28 18:05:59 +0300

    tune; get user files

--- src/DropboxAuth.js
+++ src/DropboxAuth.js
@@ -3,60 +3,46 @@
 
 import React, {Component} from "../utils/react-tuned"
 import { View, Text, TouchableOpacity, Linking } from 'react-native'
-import {Dropbox, DropboxAuth} from 'dropbox'
-import config from '../config/config'
 import auth from './auth'
 
 
 export default class extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
     this.subscribeTo(auth)
   }
 
   componentDidMount() {
-    this.init()
+    auth.getAuthUrl()
   }
 
   render() {
     return (
       <View>
-        {this.authUrl()}
+        {auth.isAuthenticated ?
+          this.files() : this.authUrl()}
       </View>
     )
   }
 
   authUrl() {
-    if (!this.state.authUrl || auth.isAuthenticated) return
+    if (!auth.authUrl) return
 
     return (
       <TouchableOpacity
         style={{margin: 20}}
         activeOpacity={1}
-        onPress={() => Linking.openURL(this.state.authUrl)}
+        onPress={() => Linking.openURL(auth.authUrl)}
       >
         <Text style={{fontSize: 20}}>Authenticate</Text>
       </TouchableOpacity>
     )
   }
 
-  async init() {
-    // todo move logic to service
-    if (auth.isAuthenticated) {
-      let dropbox = new Dropbox({ accessToken: auth.token })
-      dropbox.filesListFolder({path: ''})
-        .then(function(response) {
-          console.log(response.result.entries)
-        })
-        .catch(function(error) {
-          console.error(error);
-        });
-    } else {
-      let dropbox = new Dropbox({ clientId: config.clientId });
-      let authUrl = await dropbox.auth.getAuthenticationUrl('insight://auth')
-      this.setState( { authUrl })
-    }
+  files() {
+    return (
+      <Text>{auth.token}</Text>
+    )
   }
 
 }
--- src/auth.js
+++ src/auth.js
@@ -2,15 +2,33 @@
  **/
 
 import ComponentController from './ComponentController'
+import {Dropbox} from "dropbox"
+import config from "../config/config"
+
 
 class Auth extends ComponentController {
 
-  authenticate(token) {
-    this.update({ token })
+  async authenticate(token) {
+    try {
+      let dropbox = new Dropbox({ accessToken: token })
+      let response = await dropbox.filesListFolder({path: ''})
+      console.log('response', response) // files: response.result.entries
+      this.update({ token })
+    } catch(err) {
+      console.error('err', err) // todo errorDialog
+    }
   }
 
   get isAuthenticated() {
     return !!this.token
+  }
+
+  async getAuthUrl() {
+    if (this.isAuthenticated || this.authUrl) return
+
+    let dropbox = new Dropbox({ clientId: config.clientId });
+    let authUrl = await dropbox.auth.getAuthenticationUrl('insight://auth')
+    this.update( { authUrl })
   }
 
 }
