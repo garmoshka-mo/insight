1cc186b07e7560f27898b530d5d8dcaa8e869c7b
klobukov <klobukov.vladimir@rambler.ru>
2021-03-28 18:36:32 +0300

    temp error handling

--- src/auth.js
+++ src/auth.js
@@ -15,7 +15,12 @@ class Auth extends ComponentController {
       console.log('response', response) // files: response.result.entries
       this.update({ token })
     } catch(err) {
-      console.error('err', err) // todo errorDialog
+      console.error('err', JSON.stringify(err))
+      /*
+        todo errorDialog
+        JSON.stringify(err) sample
+        {"name":"DropboxResponseError","status":400,"headers":{"map":{"x-dropbox-request-id":"63c8eb6be2fd46efaae5b27271c104ce","x-dropbox-response-origin":"far_remote","content-security-policy":"sandbox allow-forms allow-scripts","vary":"Accept-Encoding","content-type":"text/plain; charset=utf-8","date":"Sun, 28 Mar 2021 15:23:27 GMT","server":"envoy"}},"error":"Error in call to API function \"files/list_folder\": Your app is not permitted to access this endpoint because it does not have the required scope 'files.metadata.read'. The owner of the app can enable the scope for the app using the Permissions tab on the App Console.","line":108608,"column":19,"sourceURL":"http://localhost:8081/index.bundle?platform=android&dev=true&minify=false&app=com.insight&modulesOnly=false&runModule=true"}
+       */
     }
   }
 
