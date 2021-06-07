7a3919da37f9c4a240bc81863a0d68c0f9681f83
Daniel <daniel.kucherenko@agileengine.com>
2021-06-07 03:15:28 +0300

    LogBox.ignore

--- index.js
+++ index.js
@@ -3,10 +3,27 @@
  */
 
 import './utils/sugar'
-import {AppRegistry} from 'react-native'
+import {AppRegistry, LogBox} from 'react-native'
 import {name as appName} from './app.json'
 import settings from './src/settings' // preload
 import Dashboard from './src/DashboardView.js'
+
+LogBox.ignoreLogs([
+  'Remote debugger',
+  'Require cycle',
+  'Async Storage has been extracted',
+  'ListView is deprecated',
+  'componentWillMount',
+  "requireNativeComponent",
+  "Animated: `useNativeDriver`",
+  "componentWillUpdate",
+  "Setting DrawerLayoutAndroid",
+  "AsyncStorage has been",
+  "componentWillReceiveProps",
+  "Camera is accessing findNodeHandle",
+  "VirtualizedList",
+  "Switch:" // Switch: `onTintColor`/`thumbTintColor`/`onTintColor` is deprecated, use `trackColor` instead.
+])
 
 AppRegistry.registerComponent(appName,
   () => Dashboard
