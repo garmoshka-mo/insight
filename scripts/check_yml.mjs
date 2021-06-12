55df5e6c04da6568c241196342d5f690d32155fa
Daniel <daniel.kucherenko@agileengine.com>
2021-06-12 09:50:38 +0300

    tune check yml

--- scripts/check_yml.mjs
+++ scripts/check_yml.mjs
@@ -7,15 +7,22 @@ function parse(path) {
     content = content.replace(/\t/g, '    ')
     return yaml.load(content)
   } catch (err) {
-    console.error(path, err.message)
-    throw('yml error found')
+    console.error(path)
+    console.error(err.message)
+    throw('⛔️  yml error found')
   }
 }
 
 function parseDir(path) {
+  var files = 0, checked = 0
   fs.readdirSync(path).forEach(file => {
-    if (file.endsWith('.yml')) parse(file)
+    if (file.endsWith('.yml')) {
+      parse(path + '/' + file)
+      checked++
+    }
+    files ++
   })
+  console.log(`✅  Verified ${checked} yml from ${files} files`)
 }
 
 function handleArgs() {
@@ -24,8 +31,11 @@ function handleArgs() {
   if (args[0]) {
     if (fs.lstatSync(args[0]).isDirectory())
       parseDir(args[0])
-    else
-      parse(args[0])
+    else {
+      var data = parse(args[0])
+      console.log(JSON.stringify(data, null, 2))
+      console.log('✅  Verified')
+    }
   } else
     parseDir('./')
 }
